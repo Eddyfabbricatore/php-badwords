6fa031561244f43984865e702fb5ad88e8e11c5e
Eddyfabbricatore <eddyfabbricatore2@gmail.com>
2023-10-25 17:27:36 +0200

    [add] index.php [add] result.php

--- result.php
+++ result.php
@@ -1,3 +1,10 @@
+<?php
+$paragraph = trim($_GET['paragraph']);
+$worldToCensured = trim($_GET['worldToCensured']);
+
+$paragraphCensored = str_replace($worldToCensured, '***', $paragraph);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,23 +16,21 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 
-  <title>PHP Badwords</title>
+  <title>Result</title>
 </head>
 <body>
-  <div class="container my-3">
-    <form action="result.php" method="GET">
-      <div class="mb-3">
-        <label for="paragraph" class="form-label">Testo</label>
-        <input type="text" class="form-control" id="paragraph" name="paragraph" placeholder ="Inserisci un testo...">
-      </div>
+  <div class="container my-5">
+    <h2 class="mb-3">Testo inserito</h2>
 
-      <div class="mb-3">
-        <label for="worldToCensured" class="form-label">Parola</label>
-        <input type="text" class="form-control" id="worldToCensured" name="worldToCensured" placeholder ="Inserisci una parola da censurare...">
-      </div>
-      
-      <button type="submit" class="btn btn-primary">Invia</button>
-    </form>
+    <p class="mb-3"> <?php echo $paragraph ?> </p>
+
+    <p class="mb-3">Lunghezza del testo inserito: <?php echo strlen($paragraph) ?> </p>
+
+    <h2 class="mt-5 mb-3">Testo censurato</h2>
+
+    <p class="mb-3"> <?php echo $paragraphCensored ?> </p>
+
+    <p class="mb-3">Lunghezza del testo censurato: <?php echo strlen($paragraphCensored) ?> </p>
   </div>
 </body>
 </html>
